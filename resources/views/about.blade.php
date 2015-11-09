0d06319f9727f99f0dfb6c86063965b9002a452b
Edithwn <edithndungu88@gmail.com>
2015-11-09 15:55:13 +0300

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,170 @@
+ <!DOCTYPE html>
+<html>
+<head>
+  <title>PataRide</title>
+  <link   rel="stylesheet" type="text/css"  href="Assets/css/normalize.css">
+  <link   rel="stylesheet" type="text/css" href="Assets/css/about.css">
+  <link   rel="stylesheet" href="Assets/css/font-awesome.min.css">
+  <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+
+  <link   rel="icon" href="Assets/images/favicon(4).ico" type="image/x-icon">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+ <link href='https://fonts.googleapis.com/css?family=Ubuntu:400,500' rel='stylesheet' type='text/css'>
+ <link href='https://fonts.googleapis.com/css?family=Slabo+27px' rel='stylesheet' type='text/css'>
+
+<!-- Bootstrap -->
+</head>
+<body>   
+         
+
+  <header>
+   <div class="logo" >
+    <img src="Assets/images/logo.png" >
+   </div>
+   
+
+   <div id="navigation">
+
+      <a href="#about"> <button type="button">ABOUT</button> </a>
+      <a href="#panel2"> <button type="button">DOWNLOAD APP</button> </a>
+      <a href="#keepintouch"> <button type="button">KEEP IN TOUCH</button> </a>
+
+    </div>
+
+    
+  </header>
+
+<div id="container">
+   <div id="slider">
+
+      <div id="tagline">
+        <h2>DIAL *127#. CONNECT. GET A RIDE.</h2>
+      </div>
+      <div id="ussd"> <img src="Assets/images/phoney.png">
+
+      </div>
+
+    </div>
+          
+
+      <div id="about" >   
+                       
+          <div id="panel1">
+            <img src="Assets/images/phone1.png" height="580" width="650">
+          </div>
+
+          <div id="panel2">
+
+             
+             <h1>Easy to use PataRide application</h1>
+             <h2>Save time, be on the move</h2>
+                
+             <p>The PataRide app is easy to access and use. Download it from the google play store, fill in your name, email address and a passsword within the fields.You will then have access to your account as a member of PataRide. </p>
+                <br>
+
+            <span>
+               <a href="https://play.google.com/store/apps/new"><img id="gog" src="Assets/images/googleplay.png" height="50" width="140"></a>
+            </span>
+
+          </div>
+
+
+      </div>
+
+          <div id="app">
+
+                <div id="app1">
+                  <img src="Assets/images/phone2.png" height="600" width="650">
+                </div>
+
+               <div id="app2">
+                 <h2> Easily connect with motorcyclists </h2>
+                 After loging in, enter your pick-location and destination. On the map screen, you will<br> get to see your current location and the available cyclists around your area.<br>
+                 A screen will appear with the amount of money required to be paid through M-pesa. <br> A motorcyclists is sent your way within and you can enjoy your ride.
+               </div>
+          </div>
+
+               <div id="mission">
+
+                <div class="mission">
+                      <h2>Mission</h2>
+                      To enable boda boda customers find an easier and safer way to <br> connect with the cyclists.</div>
+
+                  <div class="vision">
+
+                    <h2>Vision</h2>
+                    To enhance reliability, safety and good motorcycle transport.
+                  </div>
+                </div>
+
+
+            <div id="keepintouch">
+           
+              <div class="map">
+                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2409.8955916166788!2d36.7886879483737!3d-1.2991609990477915!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMcKwMTcnNTcuMCJTIDM2wrA0NycyNy4yIkU!5e1!3m2!1sen!2ske!4v1444739151872" width="600" height="420" frameborder="0" style="border:0" allowfullscreen></iframe>
+              </div>
+
+
+              <div class="form">
+              
+                <form>
+                 <h4>Contact us</h4>
+               
+                   <div id="name">
+                     <label for="name">Name:</label>
+                     <input type="text" id="name" />
+                   </div>
+
+
+                   <div id="email">
+                     <label for="mail">E-mail:</label>
+                     <input type="email" id="mail" />
+                   </div>
+
+
+                   <div id="message">
+                     <label for="msg">Message:</label>
+                     <textarea id="msg"></textarea>
+                   </div>
+            
+                   <div id="submit">
+                     <button type="button">Submit</button> 
+                   </div>
+             
+           
+                </form>     
+           
+              </div>
+
+        <br clear="all"> 
+
+
+        <div id="icons">
+             <a href="https://www.facebook.com/"><i class="fa fa-facebook-square fa-2x"></i></a>
+             <a href="https://twitter.com/"><i class="fa fa-twitter-square fa-2x"></i></a>
+             <a href="https://www.pinterest.com/"><i class="fa fa-pinterest-square fa-2x"></i></a>
+
+        </div>
+
+        </div>
+</div>
+
+        <footer>
+
+           +254 722 000000 | +254 70390940 | pataridejiell@gmail.com <br> <p> Privacy and policy
+           | All rights reserved | copyright2015 </p>
+        </footer>
+   
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+       <!-- Include all compiled plugins (below), or include individual files as needed -->
+            <script src="js/bootstrap.min.js"></script>  
+
+   
+                  
+</body>
+</htmL>
+  
+
